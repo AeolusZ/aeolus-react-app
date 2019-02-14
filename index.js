f627e2f65e58cbb26ed97adcf465d9a9f1b1dbcd
zrx <18202764523@163.com>
2019-02-14 15:04:24 +0800

    变量的解构赋值

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ import {BrowserRouter, Route} from 'react-router-dom'
 
 import InCome from './src'
 import FormPractice from './src/FormPractice'
+import Practice from './src/practice'
 
 ReactDOM.render(
   <BrowserRouter>
     <div>
+    {console.log(document.getElementById('app'))}
       <Route exact path='/' component={InCome}/>
-      <Route path='/formPractice' component={FormPractice}/>  
+      {/* <Route path='/formPractice' component={FormPractice}/>  */}
+      <Route path='/practice' component={Practice}/>  
     </div>
   </BrowserRouter>,
   document.getElementById('app')
 )
+console.log(document.getElementById('app'))
